d5c22cc50e43e42868dfe3702eafda3d62278312
Andres Castaneda <acastanedaa@worldbank.org>
2019-06-03 12:55:53 -0400

    fix kihoon's file format

--- building.R
+++ building.R
@@ -1,5 +1,6 @@
 
 # preview just one chapter
+file <- "Collaboration_in_Git.Rmd"
 file <- "Handover.Rmd"
 bookdown::render_book(file, "bookdown::gitbook",
                       preview = TRUE)
@@ -9,5 +10,5 @@ bookdown::render_book("index.Rmd", "bookdown::gitbook")
 
 #serve the whole book to see modification on trshe fly
 dir <- getwd()
-bookdown::serve_book(dir = dir, output_dir = "_book",
+bookdown::serve_book(dir = dir, output_dir = "docs",
                      preview = TRUE)
