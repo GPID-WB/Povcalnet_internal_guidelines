41e44d3f814963bea8bc84e5686563ba66b7d874
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-11-23 11:26:00 -0500

    complete book after organizing files

---
+++ R/building.R
@@ -0,0 +1,21 @@
+# preview just one chapter
+# file <- "Handover.Rmd"
+# file <- "DM_Group_data.Rmd"
+# file <- "Folder_strcuture.Rmd"
+# file <- "prepare_data_for_update.Rmd"
+# file <- "Collaboration_in_Git.Rmd"
+#
+# file <- "data_visualization_flourish.Rmd"
+# file <- "primus.Rmd"
+
+file <- "pcb_files.Rmd"
+bookdown::render_book(file, "bookdown::gitbook",
+                      preview = TRUE)
+
+# render the whole book
+bookdown::render_book("index.Rmd", "bookdown::gitbook")
+
+#serve the whole book to see modification on the fly
+# dir <- getwd()
+# bookdown::serve_book(dir = dir, output_dir = "docs",
+#                      preview = TRUE)
